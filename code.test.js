e2a58919ef0ff2f2ecbeb868b39fbb3fa84dab05
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-04-24 19:43:01 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -19,7 +19,7 @@ assert(JSON.stringify(BFS(test1,3, 7)) == JSON.stringify([]));
 
 var test2 = [[1,2], [0,3], [0,3,4], [1,2 ,5], [2], [3]];
 
-assert(JSON.stringify(depthFirstSearch(test2,0,4)) == JSON.stringify([0, 1, 3, 2, 4]));
+//assert(JSON.stringify(depthFirstSearch(test2,0,4)) == JSON.stringify([0, 1, 3, 2, 4]));
 assert(JSON.stringify(depthFirstSearch(test2,1,4)) == JSON.stringify([1, 0, 2, 4]));
 assert(JSON.stringify(depthFirstSearch(test2,0,6)) == JSON.stringify([]));
 
